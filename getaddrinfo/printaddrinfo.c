b13fc8b3c703f8e7d9034ff9b1d0c1ffebe13458
Claes Lillieskold <claes.lillieskold@gmail.com>
2011-10-17 17:03:44 +0200

    Added a nicer way of printing flags.

--- getaddrinfo/printaddrinfo.c
+++ getaddrinfo/printaddrinfo.c
@@ -6,54 +6,128 @@
 
 #include "printaddrinfo.h"
 
+#define STR(x) #x
+#if 0
+#define STRSTR(x) STR(x)
+#endif
+
+#define ARRAY_SIZE(array) (sizeof(array) / sizeof(*(array)))
+
+struct flags_and_desc
+{
+        uint32_t flag;
+        const char *name;
+        const char *desc;
+};
+
+static const struct flags_and_desc ai_flags_and_desc[] =
+{
+        {AI_PASSIVE, STR(AI_PASSIVE),
+         "Socket address is intended for bind"},
+        {AI_CANONNAME, STR(AI_CANONNAME),
+         "Request for canonical name"},
+        {AI_NUMERICHOST, STR(AI_NUMERICHOST),
+         "Don't use name resolution"},
+        {AI_V4MAPPED, STR(AI_V4MAPPED),
+         "IPv4 mapped addresses are acceptable"},
+        {AI_ALL, STR(AI_ALL),
+         "Return IPv4 mapped and IPv6 addresses"},
+        {AI_ADDRCONFIG, STR(AI_ADDRCONFIG),
+         "Use configuration of this host to choose returned address type"},
+        {AI_IDN, STR(AI_IDN),
+         "IDN encode input (assuming it is encoded in the current "
+         "locale's character set) before looking it up. "},
+        {AI_CANONIDN, STR(AI_CANONIDN),
+         "Translate canonical name from IDN format. "},
+        {AI_IDN_ALLOW_UNASSIGNED, STR(AI_IDN_ALLOW_UNASSIGNED),
+         "Don't reject unassigned Unicode code points"},
+        {AI_IDN_USE_STD3_ASCII_RULES, STR(AI_IDN_USE_STD3_ASCII_RULES),
+         "Validate strings according to STD3 rules"},
+};
+
 static void
 present_failed_status(int status)
 {
         switch (status)
         {
         case EAI_ADDRFAMILY:
-                printf("clli: Failed with status: EAI_ADDRFAMILY.\n");
+                printf("Failed with status: EAI_ADDRFAMILY.\n");
                 break;
         case EAI_AGAIN:
-                printf("clli: Failed with status: EAI_AGAIN.\n");
+                printf("Failed with status: EAI_AGAIN.\n");
                 break;
         case EAI_BADFLAGS:
-                printf("clli: Failed with status: EAI_BADFLAGS.\n");
+                printf("Failed with status: EAI_BADFLAGS.\n");
                 break;
         case EAI_FAIL:
-                printf("clli: Failed with status: EAI_FAIL.\n");
+                printf("Failed with status: EAI_FAIL.\n");
                 break;
         case EAI_FAMILY:
-                printf("clli: Failed with status: EAI_FAMILY.\n");
+                printf("Failed with status: EAI_FAMILY.\n");
                 break;
         case EAI_MEMORY:
-                printf("clli: Failed with status: EAI_MEMORY.\n");
+                printf("Failed with status: EAI_MEMORY.\n");
                 break;
         case EAI_NODATA:
-                printf("clli: Failed with status: EAI_NODATA.\n");
+                printf("Failed with status: EAI_NODATA.\n");
                 break;
         case EAI_NONAME:
-                printf("clli: Failed with status: EAI_NONAME.\n");
+                printf("Failed with status: EAI_NONAME.\n");
                 break;
         case EAI_SERVICE:
-                printf("clli: Failed with status: EAI_SERVICE.\n");
+                printf("Failed with status: EAI_SERVICE.\n");
                 break;
         case EAI_SOCKTYPE:
-                printf("clli: Failed with status: EAI_SOCKTYPE.\n");
+                printf("Failed with status: EAI_SOCKTYPE.\n");
                 break;
         case EAI_SYSTEM:
-                printf("clli: Failed with status: EAI_SYSTEM.\n");
+                printf("Failed with status: EAI_SYSTEM.\n");
                 break;
         default:
-                printf("clli: Failed with unknown status.\n");
+                printf("Failed with unknown status.\n");
                 break;
         }
 }
 
-static void present_ai_flags(int ai_flags)
+static void
+present_flags_and_desc(const char *msg, uint32_t flags,
+                       const struct flags_and_desc *flags_and_desc,
+                       size_t length)
 {
-        printf("clli: addrinfo->ai_flags = ");
-        switch (ai_flags)
+        unsigned int i;
+
+        for (i = 0; i < length; i++)
+        {
+                const struct flags_and_desc *curr = &flags_and_desc[i];
+
+                if (flags & curr->flag)
+                {
+                        printf("%s%s - %s.\n", msg, curr->name, curr->desc);
+                }
+        }
+}
+
+static void present_ai_flags(int order, int ai_flags)
+{
+        char msg1[100];
+        char msg2[100];
+
+        sprintf(msg1, "[%d]->ai_flags = ", order);
+        sprintf(msg2, "%-20s", msg1);
+        present_flags_and_desc(msg2, ai_flags, ai_flags_and_desc,
+                               ARRAY_SIZE(ai_flags_and_desc));
+}
+
+static void present_ai_family(int order, int ai_family)
+{
+        char msg1[100];
+        char msg2[100];
+
+        sprintf(msg1, "[%d]->ai_family = ", order);
+        sprintf(msg2, "%-20s", msg1);
+        printf("%s", msg2);
+        
+        switch (ai_family)
         {
         case AF_INET:
                 printf("AF_INET");
@@ -71,38 +145,38 @@ static void present_ai_flags(int ai_flags)
         printf("\n");
 }
 
-static void present_ai_family(int ai_family)
+static void present_ai_socktype(int order, int ai_socktype)
 {
 }
 
-static void present_ai_socktype(int ai_socktype)
+static void present_ai_protocol(int order, int ai_protocol)
 {
 }
 
-static void present_ai_protocol(int ai_protocol)
+static void present_ai_addrlen(int order, size_t ai_addrlen)
 {
 }
 
-static void present_ai_addrlen(size_t ai_addrlen)
-{
-}
-
-static void present_ai_canonname(char *ai_canonname)
+static void present_ai_canonname(int order, char *ai_canonname)
 {
 }
 
 static void present_addr_info(const struct addrinfo *addrinfo)
 {
-        present_ai_flags(addrinfo->ai_flags);
-        present_ai_family(addrinfo->ai_family);
-        present_ai_socktype(addrinfo->ai_socktype);
-        present_ai_protocol(addrinfo->ai_protocol);
-        present_ai_addrlen(addrinfo->ai_addrlen);
-        present_ai_canonname(addrinfo->ai_canonname);
+        static int order = 0;
+        
+        present_ai_flags(order, addrinfo->ai_flags);
+        present_ai_family(order, addrinfo->ai_family);
+        present_ai_socktype(order, addrinfo->ai_socktype);
+        present_ai_protocol(order, addrinfo->ai_protocol);
+        present_ai_addrlen(order, addrinfo->ai_addrlen);
+        present_ai_canonname(order, addrinfo->ai_canonname);
 
         if (addrinfo->ai_next != NULL)
         {
-                present_addr_info(addrinfo);
+                printf("\n");
+                order++;
+                present_addr_info(addrinfo->ai_next);
         }
 }
 
@@ -111,6 +185,7 @@ void print_addr_info(const char *address)
         struct addrinfo *res;
         int status;
 
+        printf("Printing addrinfo for address: \"%s\".\n", address);
         status = getaddrinfo(address, NULL, NULL, &res);
         if (status != 0)
         {
