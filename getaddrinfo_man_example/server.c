83fa8f3bd36f9647b56a0e35d4854f1a6ef24905
Claes Lillieskold <claes.lillieskold@gmail.com>
2011-10-19 14:51:45 +0200

    Rewritten the server to functions and commented. Purpose: To understand.

--- getaddrinfo_man_example/server.c
+++ getaddrinfo_man_example/server.c
@@ -8,25 +8,32 @@
 
 #define BUF_SIZE 500
 
-int
-main(int argc, char *argv[])
+/* This program implements an echo server.
+ * The function getaddrinfo is used in conjunction with the specified
+ * port to be able to create a localhost datagram socket and bind to it.
+ * Using this socket UDP-messages are received with recvfrom, and
+ * echoed back using send.
+ *
+ * Standard functions used (and what for):
+ *   getaddrinfo    - Get address info, in this case only datagram
+ *                    on a local-host port.
+ *   socket         - To create a socket on one of the results from
+ *                    getaddrinfo.
+ *   bind           - To be able to receive and send from the opened socket.
+ *   recvfrom       - Blocking receive from the socket from a peer.
+ *                    This function also provides enough information on
+ *                    the sender to be able to send back received data.
+ *   getnameinfo    - Get name of peer and port and print this.
+ *   send           - Used to send back received data to peer.
+ *
+ */
+static int get_addrinfo_on_port(struct addrinfo **result, const char *port)
 {
+        const char *node = NULL; /* Means loopback interface. */
         struct addrinfo hints;
-        struct addrinfo *result, *rp;
-        int sfd, s;
-        struct sockaddr_storage peer_addr;
-        char buf[BUF_SIZE];
-        char *port;
+        int status;
 
-        if (argc != 2)
-        {
-                fprintf(stderr, "Usage: %s port\n", argv[0]);
-                exit(__LINE__);
-        }
-        port = argv[1];
-
-        /* Setup a hint to allow IPv4 or IPv6 with only datagram
-           sockets. */
+        /* Setup a hint to allow IPv4 or IPv6 with only datagram sockets. */
         memset(&hints, 0, sizeof(struct addrinfo));
         hints.ai_family = AF_UNSPEC; /* Allow IPv4 or IPv6. */
         hints.ai_socktype = SOCK_DGRAM; /* Datagram socket. */
@@ -36,41 +43,121 @@ main(int argc, char *argv[])
         hints.ai_addr = NULL;
         hints.ai_next = NULL;
 
-        s = getaddrinfo(NULL, port, &hints, &result);
-        if (s != 0)
+        /* Request address info on localhost, given port and only
+         * datagram sockets.
+         */
+        status = getaddrinfo(node, port, &hints, result);
+        if (status != 0)
         {
-                fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-                exit(__LINE__);
+                fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+                return __LINE__;
         }
 
-        /* getaddrinfo() returns a list of address structures.
-         * Try each address until we successfully bind(2).
-         * If socket(2) (or bind(2)) fails, we (close the socket)
-         * try the next address.
-         */
-        for (rp = result; rp != NULL; rp = rp->ai_next)
-        {
-                sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        return 0;
+}
 
+static int get_bound_socket(struct addrinfo *addrinfo, int *result)
+{
+        struct addrinfo *curr;
+        int sfd;
+
+        for (curr = addrinfo; curr != NULL; curr = curr->ai_next)
+        {
+                sfd = socket(curr->ai_family, curr->ai_socktype,
+                                curr->ai_protocol);
                 if (sfd == -1)
                 {
-                        continue; /* Failed. */
+                        continue;
                 }
 
-                if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0)
+                if (bind(sfd, curr->ai_addr, curr->ai_addrlen) == 0)
                 {
-                        break; /* Succeeded. */
+                        *result = sfd;
+                        return 0;
                 }
 
                 close(sfd);
         }
 
-        if (rp == NULL)
+        return __LINE__;
+}
+
+static void print_name_info(struct sockaddr_storage peer_addr,
+                            socklen_t peer_addr_len)
+{
+        char host[NI_MAXHOST];
+        char service[NI_MAXSERV];
+        int status;
+        
+        status = getnameinfo((struct sockaddr *) &peer_addr, peer_addr_len,
+                             host, NI_MAXHOST, service, NI_MAXSERV,
+                             NI_NUMERICSERV);
+        if (status != 0)
         {
-                fprintf(stderr, "Could not bind.\n");
+                fprintf(stderr, "getnameinfo: %s.\n", gai_strerror(status));
+        }
+                
+        printf("Received from %s:%s.\n", host, service);
+}
+
+static int echo(int sfd)
+{
+        struct sockaddr_storage peer_addr;
+        char buf[BUF_SIZE];
+        socklen_t peer_addr_len = sizeof(struct sockaddr_storage);
+        ssize_t nread;
+        int status;
+
+        /* Receive from socket. */
+        nread = recvfrom(sfd, buf, BUF_SIZE, 0, (struct sockaddr *)&peer_addr,
+                         &peer_addr_len);
+        if (nread == -1)
+        {
+                return 0; /* Received nothing. */
+        }
+
+        /* Print information about the sending peer. */
+        print_name_info(peer_addr, peer_addr_len);
+ 
+        /* Send back the information to the peer. */
+        status = sendto(sfd, buf, nread, 0, (struct sockaddr*)&peer_addr,
+                        peer_addr_len);
+        if (status != nread)
+        {
+                fprintf(stderr, "sendto: %s.\n", gai_strerror(status));
                 exit(__LINE__);
         }
 
+        return 0;
+}
+
+int main(int argc, char *argv[])
+{
+        struct addrinfo *result;
+        int sfd;
+        int status;
+        char *port;
+
+        if (argc != 2)
+        {
+                fprintf(stderr, "Usage: %s port\n", argv[0]);
+                exit(__LINE__);
+        }
+
+        /* Get address info on the specified port on localhost. */
+        port = argv[1];
+        status = get_addrinfo_on_port(&result, port);
+        if (status != 0)
+        {
+                exit(status);
+        }
+
+        /* Get a bound socket to one of the addrinfo:s. */
+        status = get_bound_socket(result, &sfd);
+        if (status != 0)
+        {
+                exit(status);
+        }
         freeaddrinfo(result);
 
         /* We have now successfully opened a datagram socket, and
@@ -78,39 +165,10 @@ main(int argc, char *argv[])
          */
         for (;;)
         {
-                socklen_t peer_addr_len = sizeof(struct sockaddr_storage);
-                ssize_t nread;
-
-                nread = recvfrom(sfd, buf, BUF_SIZE, 0,
-                                 (struct sockaddr *)&peer_addr, &peer_addr_len);
-                if (nread == -1)
+                status = echo(sfd);
+                if (status != 0)
                 {
-                        continue; /* Received nothing. */
-                }
-
-                /* Get information about the sending peer. */
-                char host[NI_MAXHOST];
-                char service[NI_MAXSERV];
-                s = getnameinfo((struct sockaddr *) &peer_addr,
-                                peer_addr_len, host, NI_MAXHOST,
-                                service, NI_MAXSERV, NI_NUMERICSERV);
-                if (s != 0)
-                {
-                        fprintf(stderr, "getnameinfo: %s.\n", gai_strerror(s));
-                        exit(__LINE__);
-                }
-                
-                printf("Received %ld bytes from %s:%s.\n", (long)nread, host,
-                       service);
-
-                /* Send back the information to the peer. */
-                s = sendto(sfd, buf, nread, 0, (struct sockaddr*)&peer_addr,
-                           peer_addr_len);
-                if (s != nread)
-                {
-                        fprintf(stderr, "sendto: %s.\n", gai_strerror(s));
-                        exit(__LINE__);
+                        exit(status);
                 }
         }
 }
-        
